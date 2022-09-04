db58a54b18b4decfd7715d201367fab22ca678ee
DjK <karwowski.d@o2.pl>
2022-09-05 00:07:05 +0200

    Add IWeatherService implementation

--- WeatherApp.Core/Services/WeatherService.cs
+++ WeatherApp.Core/Services/WeatherService.cs
@@ -1,9 +1,8 @@
 ﻿using DjK.WeatherApp.Core.Models;
 using DjK.WeatherApp.Core.Services.Abstractions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace DjK.WeatherApp.Core.Services
@@ -12,37 +11,83 @@ namespace DjK.WeatherApp.Core.Services
     {
         private readonly IRestService _restService;
 
+        /// <summary>
+        /// Creates OpenWeatherService instance.
+        /// </summary>
+        /// <param name="restService">IRestService implementation.</param>
+        /// <exception cref="ArgumentNullException"></exception>
         public OpenWeatherService(IRestService restService)
         {
             _restService = restService ?? throw new ArgumentNullException(nameof(restService));
         }
 
+        /// <summary>
+        /// Sends GET request to Open Weather API to retrieve the weather data for the specified city.
+        /// </summary>
+        /// <param name="cityName">City for which the weather data are requested.</param>
+        /// <returns>WeatherResponse object. If request was not successful, WetherDetails object is null.</returns>
         public async Task<WeatherResponse> GetWeatherResponseForLocation(string cityName)
         {
             var uri = BuildRequestUri(cityName);
             var response = await _restService.GetHttpResponseMessage(uri);
             var reasonPhrase = response.ReasonPhrase;
+            var content = await response.Content.ReadAsStringAsync();
 
             if (response.IsSuccessStatusCode)
             {
-                var content = await response.Content.ReadAsStringAsync();
-                var weatherDetails = ParseResponseContent(content);
+                var weatherDetails = ParseResponseContent(content, WeatherDataParser);
                 return new WeatherResponse(weatherDetails, reasonPhrase, isSuccessful: true);
             }
             else
             {
-                return new WeatherResponse(weatherDetails: default, reasonPhrase, isSuccessful: false);
+                var message = ParseResponseContent(content, UnsuccessfulResponseParser);
+                return new WeatherResponse(
+                    weatherDetails: default,
+                    reasonPhrase: $"{reasonPhrase}: {message}",
+                    isSuccessful: false);
             }
         }
 
-        private WeatherDetails ParseResponseContent(string content)
+        private Func<JObject, string> UnsuccessfulResponseParser =>
+            message => message["message"].ToString();
+
+        private Func<JObject, WeatherDetails> WeatherDataParser =>
+            weatherData => new WeatherDetails()
+                {
+                    CityName = weatherData["name"].ToString(),
+                    Description = weatherData["weather"][0]["description"].ToString(),
+                    Temperature = Convert.ToDouble(weatherData["main"]["temp"].ToString()),
+                    TemperatureFeelsLike = Convert.ToDouble(weatherData["main"]["feels_like"].ToString()),
+                    TemperatureMin = Convert.ToDouble(weatherData["main"]["temp_min"].ToString()),
+                    TemperatureMax = Convert.ToDouble(weatherData["main"]["temp_max"].ToString())
+                };
+
+        private TResult ParseResponseContent<TResult>(string message, Func<JObject, TResult> contentParser)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var content = JObject.Parse(message);
+                return contentParser(content);
+            }
+            catch (JsonReaderException)
+            {
+                // TODO: add logging
+                throw;
+            }
+            catch (Exception)
+            {
+                // TODO: add logging
+                throw;
+            }
         }
 
         private string BuildRequestUri(string cityName)
         {
-            throw new NotImplementedException();
+            // TODO: customize request based on CultureInfo
+            // CultureInfo cultureInfo = CultureInfo.GetCultureInfo();
+
+            return $@"{Constants.Constants.OpenWeatherMapEndpoint}?q={cityName}&appid={Constants.Constants.OpenWeatherMapAPIKey}";
+
         }
     }
 }
