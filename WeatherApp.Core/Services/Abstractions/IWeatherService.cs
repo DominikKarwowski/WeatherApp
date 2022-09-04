f0a6ffd22c4d711937d5e47e28d1bb23c0821c48
DjK <karwowski.d@o2.pl>
2022-09-04 20:35:47 +0200

    Add weather service abstraction

---
+++ WeatherApp.Core/Services/Abstractions/IWeatherService.cs
@@ -0,0 +1,13 @@
+﻿using DjK.WeatherApp.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DjK.WeatherApp.Core.Services.Abstractions
+{
+    public interface IWeatherService
+    {
+        Task<WeatherDetails> GetWeatherDetailsForLocation(string cityName);
+    }
+}
