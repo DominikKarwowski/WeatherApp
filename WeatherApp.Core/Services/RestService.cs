d6d6f848ddd2a8983e01db14a4304b98312d6068
DjK <karwowski.d@o2.pl>
2022-09-05 00:06:41 +0200

    Add IRestService implementation

---
+++ WeatherApp.Core/Services/RestService.cs
@@ -0,0 +1,21 @@
+﻿using DjK.WeatherApp.Core.Services.Abstractions;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DjK.WeatherApp.Core.Services
+{
+    public class RestService : IRestService
+    {
+        private readonly HttpClient _httpClient;
+
+        public RestService()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public Task<HttpResponseMessage> GetHttpResponseMessage(string requestUri)
+        {
+            return _httpClient.GetAsync(requestUri);
+        }
+    }
+}
