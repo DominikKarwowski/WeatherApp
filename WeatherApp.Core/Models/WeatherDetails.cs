87887dec6abb2084a019a38e9129b25bbc250db4
DjK <karwowski.d@o2.pl>
2022-09-04 14:37:55 +0200

    Add WeatherDetails model

---
+++ WeatherApp.Core/Models/WeatherDetails.cs
@@ -0,0 +1,34 @@
+﻿namespace DjK.WeatherApp.Core.Models
+{
+    /// <summary>
+    /// Contains weather data.
+    /// </summary>
+    public class WeatherDetails
+    {
+        /// <summary>
+        /// Weather condition description.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Temperature.
+        /// </summary>
+        public float Temperature { get; set; }
+
+        /// <summary>
+        /// Temperature according to the human perception of weather.
+        /// </summary>
+        public float TemperatureFeelsLike { get; set; }
+
+        /// <summary>
+        /// Minimum temperature at the moment.
+        /// </summary>
+        public float TemperatureMin { get; set; }
+
+        /// <summary>
+        /// Maximum temperature at the moment.
+        /// </summary>
+        public float TemperatureMax { get; set; }
+
+    }
+}
