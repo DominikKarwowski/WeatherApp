8199a35330116a7901a5aff369f55cc93c227ed9
DjK <karwowski.d@o2.pl>
2022-09-04 16:41:04 +0200

    Add user input handling to HomeView
    * Create components for user interaction
    * Handle user interaction with underlying viewModel

--- WeatherApp.Core/ViewModels/HomeViewModel.cs
+++ WeatherApp.Core/ViewModels/HomeViewModel.cs
@@ -1,6 +1,9 @@
-﻿using MvvmCross.Navigation;
+﻿using DjK.WeatherApp.Core.Models;
+using MvvmCross.Commands;
+using MvvmCross.Navigation;
 using MvvmCross.ViewModels;
 using System;
+using System.Threading.Tasks;
 
 namespace DjK.WeatherApp.Core.ViewModels
 {
@@ -8,9 +11,36 @@ namespace DjK.WeatherApp.Core.ViewModels
     {
         private readonly IMvxNavigationService _navigationService;
 
+        private string city;
+
+        public string City
+        {
+            get { return city; }
+            set { SetProperty(ref city, value); }
+        }
+
+
+        public IMvxAsyncCommand ShowWeatherDetailsCommand => new MvxAsyncCommand(ShowWeatherDetails);
+
+
         public HomeViewModel(IMvxNavigationService navigationService)
         {
             _navigationService = navigationService ?? throw new ArgumentNullException(nameof(navigationService));
+        }
+
+        private async Task ShowWeatherDetails()
+        {
+            //TODO: get actual WeatherDetails based on the provided city and replace dummy WeatherDetails
+            var weatherDetails = new WeatherDetails()
+            {
+                Description = $"dummy weather description for {City}",
+                Temperature = 100,
+                TemperatureFeelsLike = 99,
+                TemperatureMax = 103,
+                TemperatureMin = 97
+            };
+
+            await _navigationService.Navigate(typeof(WeatherDetailsViewModel), weatherDetails);
         }
     }
 }
