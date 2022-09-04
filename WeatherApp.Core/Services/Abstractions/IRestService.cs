c4b4c2753fa3cd9a0773306f7d43b7dcefa80b25
DjK <karwowski.d@o2.pl>
2022-09-04 22:07:23 +0200

    Add REST service abstraction

---
+++ WeatherApp.Core/Services/Abstractions/IRestService.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DjK.WeatherApp.Core.Services.Abstractions
+{
+    public interface IRestService
+    {
+        Task<HttpResponseMessage> GetHttpResponseMessage(string requestUri);
+    }
+}
