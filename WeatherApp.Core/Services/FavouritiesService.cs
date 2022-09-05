7b261d5094997b33d8d28d613251824e69674e48
DjK <karwowski.d@o2.pl>
2022-09-05 02:57:06 +0200

    Add favourities service

---
+++ WeatherApp.Core/Services/FavouritiesService.cs
@@ -0,0 +1,27 @@
+﻿using DjK.WeatherApp.Core.Repository.Abstractions;
+using System;
+using System.Threading.Tasks;
+
+namespace DjK.WeatherApp.Core.Services
+{
+    public class FavouritiesService : IFavouritiesService
+    {
+        private readonly ICityRepository _cityRepository;
+
+        public FavouritiesService(ICityRepository cityRepository)
+        {
+            _cityRepository = cityRepository ?? throw new ArgumentNullException(nameof(cityRepository));
+        }
+
+        public async Task SaveFavouriteCity(string cityName)
+        {
+            await _cityRepository.SaveFavouriteCity(cityName);
+        }
+
+        public async Task<string> LoadFavouriteCity()
+        {
+            return await _cityRepository.LoadFavouriteCity();
+        }
+
+    }
+}
