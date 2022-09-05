f1cb012a5232e202bcec256b8d73796c30b4457a
DjK <karwowski.d@o2.pl>
2022-09-05 02:36:43 +0200

    Add city repository

---
+++ WeatherApp.Core/Repository/Abstractions/ICityRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DjK.WeatherApp.Core.Repository.Abstractions
+{
+    public interface ICityRepository
+    {
+        Task SaveFavouriteCity(string cityName);
+    }
+}
---
+++ WeatherApp.Core/Repository/CityRepository.cs
@@ -0,0 +1,16 @@
+﻿using DjK.WeatherApp.Core.Repository.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DjK.WeatherApp.Core.Repository
+{
+    public class CityRepository : ICityRepository
+    {
+        public Task SaveFavouriteCity(string cityName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
