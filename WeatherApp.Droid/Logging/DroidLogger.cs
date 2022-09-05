50be00940ad572568bf9530907e648b5115708f6
DjK <karwowski.d@o2.pl>
2022-09-05 18:41:01 +0200

    Add template for ILogger implementation

---
+++ WeatherApp.Droid/Logging/DroidLogger.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DjK.WeatherApp.Droid.Logging
+{
+    public class DroidLogger : ILogger
+    {
+        public IDisposable BeginScope<TState>(TState state) => default!;
+
+        public bool IsEnabled(LogLevel logLevel) => true;
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            // TODO: implement actual logging
+        }
+    }
+}
---
+++ WeatherApp.Droid/Logging/DroidLoggerFactory.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DjK.WeatherApp.Droid.Logging
+{
+    public class DroidLoggerFactory : ILoggerFactory
+    {
+        public void AddProvider(ILoggerProvider provider)
+        {
+            // TODO: implement actual logger provider
+        }
+
+        public ILogger CreateLogger(string categoryName) => new DroidLogger();
+
+        public void Dispose()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+}
