3b3e5ef3b344ab178532e4724bf7c472eba1e635
DjK <karwowski.d@o2.pl>
2022-09-05 18:41:33 +0200

    Add new test classes

---
+++ WeatherApp.Core.Tests/Services/FavouritiesServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp.Core.Tests.Services
+{
+    [TestFixture]
+    public class FavouritiesServiceTests
+    {
+
+        [Test]
+        public void Save_favourite_city_to_a_repository()
+        {
+            Assert.Fail();
+        }
+
+
+        [Test]
+        public void Load_favourite_city_from_a_repository()
+        {
+            Assert.Fail();
+        }
+
+
+
+
+    }
+}
