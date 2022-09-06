9973e3c6db938d20e69e27685fecb3beb23068ff
DjK <karwowski.d@o2.pl>
2022-09-06 22:25:49 +0200

    Add ConnectivityService
    ConnectivityService is a Xamarin.Essentials.Connectivity
    wrapper to enhance testability

---
+++ WeatherApp.Core/Services/Abstractions/IConnectivityService.cs
@@ -0,0 +1,14 @@
+﻿namespace DjK.WeatherApp.Core.Services.Abstractions
+{
+    /// <summary>
+    /// Represents a connectivity service.
+    /// </summary>
+    public interface IConnectivityService
+    {
+        /// <summary>
+        /// Represents a method to check if active internet connection is available.
+        /// </summary>
+        /// <returns>True if active internet connection is available, false otherwise.</returns>
+        bool IsActiveInternetConnection();
+    }
+}
---
+++ WeatherApp.Core/Services/ConnectivityService.cs
@@ -0,0 +1,18 @@
+﻿using DjK.WeatherApp.Core.Services.Abstractions;
+using Xamarin.Essentials;
+
+namespace DjK.WeatherApp.Core.Services
+{
+    /// <summary>
+    /// Connectivity service. Wrapper for Xamarin.Essentials.Connectivity.
+    /// </summary>
+    public class ConnectivityService : IConnectivityService
+    {
+        /// <summary>
+        /// Checks if active internet connection is available.
+        /// </summary>
+        /// <returns>True if active internet connection is available, false otherwise.</returns>
+        public bool IsActiveInternetConnection() =>
+            Connectivity.NetworkAccess == NetworkAccess.Internet;
+    }
+}
