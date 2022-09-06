89797bd278a29cf385cbb4f93c6eff3b3ad18d0b
DjK <karwowski.d@o2.pl>
2022-09-07 01:11:13 +0200

    Add API endpoint and key source file

---
+++ WeatherApp.Core/Constants/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace DjK.WeatherApp.Core.Constants
+{
+    internal static class Constants
+    {
+        internal static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        internal static string OpenWeatherMapAPIKey = ""; // API key goes here
+    }
+}
